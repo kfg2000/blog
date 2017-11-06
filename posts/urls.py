8db4890e24fde0cb5ea7b26a19d938583d975755
Khalid Al-Ghunaim <kfg2000@live.com>
2017-11-06 20:59:38 +0300

    linked lists

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^top/$', views.main_post, name='top'),
     url(r'^funny/$', views.fun, name='fun'),
     url(r'^list/$', views.post_list, name = 'list'),
-    url(r'^detail/(?P<post_id>\d+)$', views.post_detail, name = 'detail'),
+    url(r'^detail/(?P<post_id>\d+)/$', views.post_detail, name = 'detail'),
 ]
